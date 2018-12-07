64ef5bd34c24c96ba32fcfede6c3060d44a7a9bd
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-12-08 01:24:58 +0500

    Project created update 2

--- Ch19-P10.2-TPL/Program.cs
+++ Ch19-P10.2-TPL/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Ch19_P10._2_TPL
@@ -10,7 +11,53 @@ namespace Ch19_P10._2_TPL
     {
         static void Main(string[] args)
         {
+            ShowThreadInfo("Main Method");
+            //Task.Factory.StartNew(() => Method1());
+            //Task.Factory.StartNew(() => Method2());
 
+            Task.Run(() => Method1());
+            Task.Run(() => Method2());
+            
+            //Method1();
+
+            ShowThreadInfo("Main Method");
+            Console.WriteLine(" Press any key to terminate ");
+            Console.ReadLine();
+        }
+
+        private static void ShowThreadInfo(object info)
+        {
+            Console.WriteLine("_______________________________________________________________");
+            Console.WriteLine(" === Current Thread Information (  "+ info.ToString() +" ) === ");
+            Console.WriteLine(" Thread ID   = " + Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine(" ThreadState = " + Thread.CurrentThread.ThreadState);
+            Console.WriteLine("_______________________________________________________________");
+        }
+
+        private static void Method2()
+        {
+            Console.WriteLine("\n ***** Method 2 ***** ");
+            ShowThreadInfo("Method 2");
+            Console.WriteLine(" Method 2 Working... \n");
+            for (int i = 0; i < 15; i++)
+            {
+                Console.Write(" "+i);
+                Thread.Sleep(200);
+            }
+            Console.WriteLine("\n Exiting Method 2 \n");
+        }
+
+        private static void Method1()
+        {
+            Console.WriteLine("\n ***** Method 1 ***** ");
+            ShowThreadInfo(" Method 1 ");
+            Console.WriteLine(" Method 1 Working... \n");
+            for (int i = 0; i < 15; i++)
+            {
+                Console.Write(" " + i);
+                Thread.Sleep(200);
+            }
+            Console.WriteLine("\n Exiting Method 1 \n");
         }
     }
 }
