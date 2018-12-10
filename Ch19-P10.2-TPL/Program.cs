42743791101c57b0c2092dfc64212295102fe7cd
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-12-11 00:35:37 +0500

    final update from KQ

--- Ch19-P10.2-TPL/Program.cs
+++ Ch19-P10.2-TPL/Program.cs
@@ -15,14 +15,43 @@ namespace Ch19_P10._2_TPL
             //Task.Factory.StartNew(() => Method1());
             //Task.Factory.StartNew(() => Method2());
 
-            Task.Run(() => Method1());
-            Task.Run(() => Method2());
-            
+            //Task.Run(() => Method1());
+            //Task.Run(() => Method2());
+
+            //Parallel.Invoke(() => Method1(), () => Method2());
+
+            Action action1 = new Action(BossMethod1());
+            Action action2 = new Action(BossMethod2());
+            Action action3 = new Action(BossMethod3());
+            Action action4 = new Action(BossMethod4());
+
+            //Parallel.Invoke(action1 , action2 , )
+
             //Method1();
 
             ShowThreadInfo("Main Method");
             Console.WriteLine(" Press any key to terminate ");
             Console.ReadLine();
+        }
+
+        private static Action BossMethod4()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Action BossMethod3()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Action BossMethod2()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Action BossMethod1()
+        {
+            throw new NotImplementedException();
         }
 
         private static void ShowThreadInfo(object info)
